93915407e17e92daf23716cbd81021f63cc7b0cb
ting-c <ting.cheong31@gmail.com>
2020-04-23 21:34:32 +0100

    create signup feature

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
 
 class SignUp extends Component {
   state = {
@@ -16,10 +19,13 @@ class SignUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+    this.props.signUp(this.state)
   }
 
   render() {
+    const { auth } = this.props
+    if (auth.uid) return <Redirect to='/main' />
+
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -49,4 +55,16 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp
+const mapStateToProps = (state) => {
+  return {
+     auth: state.firebase.auth
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
