75ba5395b509e49c0ed23a3b3506c8303b98faab
ting-c <ting.cheong31@gmail.com>
2020-05-19 14:45:05 +0100

    added link to homepage on logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar.styles.scss';
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
@@ -9,7 +10,7 @@ import avatar from './../../img/avatar.png';
 const NavBar = () => (
 	<Navbar className="NavBar">
 		<Navbar.Brand className="logo-container">
-			<img className="logo" src={logo} alt="Logo" />
+			<Link to='/'><img className="logo" src={logo} alt="Logo" /></Link>
 		</Navbar.Brand>
 		<Navbar.Toggle />
 		<Navbar.Collapse>
